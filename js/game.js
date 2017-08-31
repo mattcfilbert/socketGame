acd0c4bbfde532467a1c356d889389654548befa
MATT FILBERT <mcfilbert@email.wm.edu>
2017-08-31 18:29:54 -0400

    lololol

--- js/game.js
+++ js/game.js
@@ -2,14 +2,14 @@ var game = new Phaser.Game(24*32, 17*32, Phaser.AUTO, document.getElementById('g
 game.state.add('Game', Game)
 game.state.start('Game')
 
-var Game = Game || {}
+var Game = {}
 
 Game.init = () => {
   game.stage.disableVisibilityChange = true
 }
 
 Game.preload = () => {
-  game.load.titlemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON)
+  game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON)
   game.load.spritesheet('tileset', 'assets/map/tilesheet.png', 32, 32)
   game.load.image('sprite', 'assets/sprites/sprite.png')
 }
@@ -19,8 +19,8 @@ Game.create = () => {
   var map = game.add.tilemap('map')
   map.addTilesetImage('tilesheet', 'tileset') // tilesheet is the key of the tileset in map's JSON file
   var layer
-  for (var i = 0; map.layers.length > 0; i++) {
-    lay = map.createLayer(i)
+  for (var i = 0; i > map.layers.length; i++) {
+    layer = map.createLayer(i)
   }
   layer.inputEnabled = true // Allows clicking on the map
   Client.askNewPlayer()
